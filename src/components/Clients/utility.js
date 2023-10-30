ce905edc19746f5a2d40b4b260b7c01733693207
kiara9393 <chiara.amato9393@gmail.com>
2023-10-30 18:16:42 +0100

    add clients section

---
+++ src/components/Clients/utility.js
@@ -0,0 +1,78 @@
+import edenred from '../../assets/img/clients-logo/ticketRestaurant-logo.png';
+import menarini from '../../assets/img/clients-logo/logo-menarini.png';
+import fideuram from '../../assets/img/clients-logo/FIDEURAM.png';
+import replay from '../../assets/img/clients-logo/REPLAY.png';
+import rcs from '../../assets/img/clients-logo/RCS.png';
+import cucinaBarilla from '../../assets/img/clients-logo/cucinabarilla.png';
+import wonders from '../../assets/img/clients-logo/WONDERS.png';
+import uta from '../../assets/img/clients-logo/UTA.png';
+import laScuolaSei from '../../assets/img/clients-logo/laScuolaSei.png';
+import fattiTrovare from '../../assets/img/clients-logo/fattiTrovare.png';
+import yeldo from '../../assets/img/clients-logo/YELDO.png';
+import botter from '../../assets/img/clients-logo/BOTTER.png';
+import previnet from '../../assets/img/clients-logo/PREVINET.png';
+import sigest from '../../assets/img/clients-logo/SIGEST.png';
+import freeTox from '../../assets/img/clients-logo/FREETOX.png';
+
+export const logos = [
+  {
+    src: edenred,
+    alt: 'Edenred logo'
+  },
+  {
+    src: menarini,
+    alt: 'Menarini logo'
+  },
+  {
+    src: fideuram,
+    alt: 'Fideuram logo'
+  },
+  {
+    src: replay,
+    alt: 'Replay logo'
+  },
+  {
+    src: rcs,
+    alt: 'RCS logo'
+  },
+  {
+    src: cucinaBarilla,
+    alt: 'Cucina Barilla logo'
+  },
+  {
+    src: wonders,
+    alt: 'Wonders logo'
+  },
+  {
+    src: uta,
+    alt: 'UTA logo'
+  },
+  {
+    src: laScuolaSei,
+    alt: 'La Scuola Sei logo'
+  },
+  {
+    src: fattiTrovare,
+    alt: 'Fatti Trovare logo'
+  },
+  {
+    src: yeldo,
+    alt: 'Yeldo logo'
+  },
+  {
+    src: botter,
+    alt: 'Botter logo'
+  },
+  {
+    src: previnet,
+    alt: 'Previnet logo'
+  },
+  {
+    src: sigest,
+    alt: 'Sigest logo'
+  },
+  {
+    src: freeTox,
+    alt: 'FreeTox logo'
+  }
+];
--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Hero from '../../components/Hero/Hero';
+import Clients from '../../components/Clients/Clients';
+import AboutMe from '../../components/AboutMe/AboutMe';
 import './Home.scss';
 
 const Home = () => (
   <div className="background-image">
     <Header />
     <Hero />
+    <Clients />
+    <AboutMe />
   </div>
 );
 
