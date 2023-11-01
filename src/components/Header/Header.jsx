423c15c9b38af0c27a435be00a32f7d2fcec6750
kiara9393 <chiara.amato9393@gmail.com>
2023-11-01 20:35:26 +0100

    wip style for tablet and mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './Header.scss';
 
 const Header = () => (
-  <div className="container">
+  <div className="header-container">
     <div className="profile-name">Beatrice Amato</div>
-    <div className="navbar">
+    <div className="nav-menu">
       <a className="nav-link">About me</a>
       <a className="nav-link">Portfolio</a>
       <a className="nav-link">Contract</a>
