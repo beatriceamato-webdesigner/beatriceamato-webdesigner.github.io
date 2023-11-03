93c97bffc50972a7d1fd4fc393f0703a7b237cf3
kiara9393 <chiara.amato9393@gmail.com>
2023-11-03 18:28:43 +0100

    wip carousel

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import Flickity from 'react-flickity-component';
+import Fideuram from '../../assets/img/carousel-clients/Fideuram.png';
+import MacBook from '../../assets/img/carousel-clients/MacBook.png';
+import Wonders from '../../assets/img/carousel-clients/Wonders.png';
+import './Carousel.scss';
+import './flickity.scss';
+
+const Carousel = () => {
+  return (
+    <div className="carousel-container">
+      <h1>My recent works</h1>
+      <Flickity>
+        <img className="carousel-img" src={Fideuram} />
+        <img className="carousel-img" src={MacBook} />
+        <img className="carousel-img" src={Wonders} />
+      </Flickity>
+    </div>
+  );
+};
+
+Carousel.propTypes = {
+  // Add here some propTypes
+};
+
+Carousel.defaultProps = {
+  // Add here some default propTypes values
+};
+
+export default Carousel;
--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../../components/Header/Header';
 import Hero from '../../components/Hero/Hero';
 import Clients from '../../components/Clients/Clients';
 import AboutMe from '../../components/AboutMe/AboutMe';
+import Carousel from '../../components/Carousel/Carousel';
 import './Home.scss';
 
 const Home = () => {
@@ -18,6 +19,7 @@ const Home = () => {
       <Hero handleClick={handleClick} />
       <Clients />
       <AboutMe />
+      <Carousel />
     </div>
   );
 };
