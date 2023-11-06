22189bc2b60650ffd6b79791c05419c4754b1c13
kiara9393 <chiara.amato9393@gmail.com>
2023-11-06 18:10:55 +0100

    change img and add style for havering into carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+// import { useNavigate } from 'react-router';
 import Flickity from 'react-flickity-component';
 import Fideuram from '../../assets/img/carousel-clients/Fideuram.png';
-import MacBook from '../../assets/img/carousel-clients/MacBook.png';
+import Edenred from '../../assets/img/carousel-clients/Edenred.png';
 import Wonders from '../../assets/img/carousel-clients/Wonders.png';
 import './Carousel.scss';
 import './flickity.scss';
 
 const Carousel = () => {
+  // const history = useNavigate();
+
   return (
     <div className="carousel-container">
       <span className="carousel-title">
         My recent <span className="carousel-title-beauty">&nbsp;works</span>
       </span>
       <Flickity>
-        <img className="carousel-img" src={Fideuram} />
-        <img className="carousel-img" src={MacBook} />
-        <img className="carousel-img" src={Wonders} />
+        {/* <img className="carousel-img" src={Fideuram} onClick={() => history('/')} /> */}
+        <div className="carousel-wrap-img">
+          <img className="carousel-img" src={Fideuram} />
+          <p className="carousel-img__description">Fideuram</p>
+        </div>
+        <div className="carousel-wrap-img">
+          <img className="carousel-img" src={Wonders} />
+          <p className="carousel-img__description">ASPI x Wonders</p>
+        </div>
+        <div className="carousel-wrap-img">
+          <img className="carousel-img" src={Edenred} />
+          <p className="carousel-img__description">Edenred</p>
+        </div>
       </Flickity>
     </div>
   );
