da85b02916f69f60741813e488216d81acf56590
kiara9393 <chiara.amato9393@gmail.com>
2023-11-07 12:10:01 +0100

    restyle services page and download CV

--- src/routes/ServicePage/ServicePage.jsx
+++ src/routes/ServicePage/ServicePage.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import Anchor from '../../components/Anchor/Anchor';
+import cv from '../../assets/CV.pdf';
 import './ServicePage.scss';
 
 function ServicePage() {
-  return <div className="servicePage-image"></div>;
+  return (
+    <div className="service-backgroung">
+      <div className="service-container">
+        <div className="service-content">
+          <span className="service-title">WORK IN PROGRESS</span>
+          <span className="service-subtitle">
+            Ciao, al momento stiamo lavorando per migliorare il nostro sito. Vi invitiamo a rimanere
+            sintonizzati per ulteriori aggiornamenti.
+          </span>
+          <Anchor
+            href={cv}
+            text="Download CV"
+            icon="bi bi-arrow-down"
+            nameDownload="CV-BeatriceAmato"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ServicePage;
